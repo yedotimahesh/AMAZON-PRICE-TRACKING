71ccbe5fb8407bdbbd1399374b033336b07c88ce
yedotimahesh <122370730+yedotimahesh@users.noreply.github.com>
2023-04-16 20:13:23 +0530

    upload1

---
+++ touch.sh
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/yedotimahesh/AMAZON-PRICE-TRACKING.git
+git remote -v  --force
+git add . --force
+git commit -m "upload1"
+
+
+git push -f origin master
